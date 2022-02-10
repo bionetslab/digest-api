1f16e75ce0da8fa74f08ede1e5751d7e40a4b943
AndiMajore <andi.majore@googlemail.com>
2022-02-10 18:51:22 +0100

    checking for necessity of updates

--- digest_backend/digest_executor.py
+++ digest_backend/digest_executor.py
@@ -1,4 +1,11 @@
+import os
+
 from setup import main as digest_setup
+from mappers.mapper import FileMapper as digest_files
+import single_validation
+# from application.setup import main as digest_setup
+# from application.mappers.mapper import FileMapper as digest_files
+
 
 
 def setup(self):
@@ -7,9 +14,19 @@ def setup(self):
 
 
 def check(self):
-    print("checking")
-    if True:
+    needed = list(digest_files.file_names.values())
+    for file in os.listdir("/usr/src/digest/mapping_files"):
+        if file in needed:
+            needed.remove(file)
+    if len(needed) > 0:
         setup(self)
+    else:
+        print("Setup fine! All files are already there.")
 
 def clear(self):
-    pass
+    for file in os.listdir("/usr/src/digest/mapping_files"):
+        os.remove("/usr/src/digest/mapping_files"+file)
+
+
+def validate(r, ri, t, ti, m):
+    single_validation()
--- digest_backend/views.py
+++ digest_backend/views.py
@@ -1,16 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+import digest_backend.digest_executor as digest_executor
 #from digest_backend.models import Example
 #from digest_backend.serializers import ExampleSerializer
 #
 #
-# @api_view(['GET'])
-# def test(request) -> Response:
-#     if request.query_params.get('nr'):
-#         example = Example.objects.get(count=request.query_params.get("nr"))
-#         resp = {'examples': ExampleSerializer().to_representation(example)}
-#     else:
-#         examples = Example.objects.all()
-#         resp = {'examples': ExampleSerializer(many=True).to_representation(examples)}
-#     return Response(resp)
+@api_view(['GET'])
+def test(request) -> Response:
+    digest_executor.single_validation()
+    # if request.query_params.get('nr'):
+        # example = Example.objects.get(count=request.query_params.get("nr"))
+        # resp = {'examples': ExampleSerializer().to_representation(example)}
+    # else:
+        # examples = Example.objects.all()
+        # resp = {'examples': ExampleSerializer(many=True).to_representation(examples)}
+    # return Response(resp)
