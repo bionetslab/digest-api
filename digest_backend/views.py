c07ea80ec7f862319e96a4e8b8afed32e0957162
AndiMajore <andi.majore@googlemail.com>
2022-02-22 21:20:58 +0100

    saving id type now

--- digest_backend/views.py
+++ digest_backend/views.py
@@ -67,7 +67,7 @@ def get_status(request)->Response:
         'status':task.status,
         'stats':task_stats(task),
         'mode':task.mode,
-        'type':"TODO"
+        'type':task.type
     })
     return response
 
