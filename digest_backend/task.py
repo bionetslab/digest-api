3b43d9067a2c5c615fd6b419fe36e67e07c7f007
AndiMajore <andi.majore@googlemail.com>
2022-02-22 19:54:15 +0100

    finalize with done

--- digest_backend/task.py
+++ digest_backend/task.py
@@ -29,7 +29,6 @@ def run_task(uid, mode, parameters):
         r.set(f'{uid}_finished_at', str(datetime.now().timestamp()))
         r.set(f'{uid}_done', '1')
         print(uid+" finished!")
-
         set_status('Done')
 
     set_status('Initialized')
@@ -50,6 +49,7 @@ def run_task(uid, mode, parameters):
             digest_backend.digest_executor.run_set_set(task_hook)
         elif mode=='cluster':
             digest_backend.digest_executor.run_cluster(task_hook)
+        r.set(f'{uid}_done','1')
     except Exception as e:
         set_status(f'{e}')
         r.set(f'{uid}_failed','1')
