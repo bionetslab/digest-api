fd679a3de97df6772ea67f47a470cb31bf3de9e5
AndiMajore <andi.majore@googlemail.com>
2022-02-22 17:39:36 +0100

    fixed uid datatype in db model

--- digest_backend/views.py
+++ digest_backend/views.py
@@ -15,6 +15,7 @@ from digest_backend.models import Task
 from digest_backend.task import start_task, refresh_from_redis, task_stats
 
 def run(mode, data) -> Response:
+    print(data)
     task = Task.objects.create(uid=data["uid"], mode=mode, parameters=data)
     start_task(task)
     task.save()
