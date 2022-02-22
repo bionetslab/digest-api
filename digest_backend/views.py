4d82aee54023523bd594ae3059abe041a5a7b75d
AndiMajore <andi.majore@googlemail.com>
2022-02-22 21:45:10 +0100

    responding with whole parameter object on status request

--- digest_backend/views.py
+++ digest_backend/views.py
@@ -67,7 +67,7 @@ def get_status(request)->Response:
         'status':task.status,
         'stats':task_stats(task),
         'mode':task.mode,
-        'type':json.loads(task.parameters)["type"]
+        'parameters':task.parameters
     })
     return response
 
