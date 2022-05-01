5892edc26c1f80fe30f7d4b81a43778a71949526
AndiMajore <andi.majore@googlemail.com>
2022-05-01 18:45:25 +0200

    fixed timezone handling

--- digest_backend/updater.py
+++ digest_backend/updater.py
@@ -4,11 +4,11 @@ from digest_backend.digest_executor import dry_setup
 
 
 def start():
-    scheduler = BackgroundScheduler("Europe/Paris")
+    scheduler = BackgroundScheduler(timezone="Europe/Paris")
     scheduler.add_jobstore(DjangoJobStore(), 'default')
     register_events(scheduler)
 
-    @scheduler.scheduled_job('cron', day_of_week="sun", hour=18, minute=45, second=0,  name='update_mapping')
+    @scheduler.scheduled_job('cron', day_of_week="sun", hour=18, minute=48, second=0,  name='update_mapping')
     def update_mappings():
         dry_setup()
 
