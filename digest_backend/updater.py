ac22a845fc505634eca019289e12523b1b0400fc
AndiMajore <andi.majore@googlemail.com>
2022-05-01 21:49:57 +0200

    changed updater call

--- digest_backend/updater.py
+++ digest_backend/updater.py
@@ -1,19 +1,19 @@
 from apscheduler.schedulers.background import BlockingScheduler
 # from apscheduler.scheduler import Scheduler
-from django_apscheduler.jobstores import register_events, DjangoJobStore
+from django_apscheduler.jobstores import DjangoJobStore, register_events, register_job
 from digest_backend.digest_executor import dry_setup
 import time
 
+scheduler = BlockingScheduler(timezone="Europe/Paris")
+scheduler.add_jobstore(DjangoJobStore(), 'djangojobstore')
 
 def start():
     print("Setting up update scheduler")
-    scheduler = BlockingScheduler(timezone="Europe/Paris")
-    scheduler.add_jobstore(DjangoJobStore(), 'djangojobstore')
-    scheduler.configure({'apscheduler.daemon':False})
-    register_events(scheduler)
 
-    @scheduler.scheduled_job(trigger='cron', day_of_week="sun", hour=21, minute=45, second=0,  name='update_mapping')
+    scheduler.configure({'apscheduler.daemon':False})
+    @register_job(scheduler,"cron", day_of_week="sun", hour=21, minute=45, second=0,  name='update_mapping')
     def update_mappings():
         dry_setup()
+    register_events(scheduler)
     print("setting scheduler start")
     scheduler.start()
