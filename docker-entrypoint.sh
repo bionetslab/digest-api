88a35549080ffc4e9299c8a6ed12ff42681b4a90
AndiMajore <andi.majore@googlemail.com>
2023-03-21 17:08:52 +0100

    fixed postgres image version

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ sleep 5
 python3 manage.py makemigrations
 python3 manage.py migrate --run-syncdb
 python3 manage.py setup_digest --check
-python3 manage.py migrate --run-syncdb
-python3 manage.py setup_db --refill
+#python3 manage.py migrate --run-syncdb
+#python3 manage.py setup_db --refill
 
 /usr/bin/supervisord -c "/etc/supervisor/conf.d/supervisord.conf"
