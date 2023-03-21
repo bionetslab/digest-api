6b1c48dd838c7eb8694fa1b08dd39753b87816e5
AndiMajore <andi.majore@googlemail.com>
2023-03-21 17:07:00 +0100

    full reset of digest-db

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,7 @@ sleep 5
 python3 manage.py makemigrations
 python3 manage.py migrate --run-syncdb
 python3 manage.py setup_digest --check
-#python3 manage.py migrate --run-syncdb
-#python3 manage.py setup_db --refill
-#bash import-example_files.sh
+python3 manage.py migrate --run-syncdb
+python3 manage.py setup_db --refill
 
 /usr/bin/supervisord -c "/etc/supervisor/conf.d/supervisord.conf"
